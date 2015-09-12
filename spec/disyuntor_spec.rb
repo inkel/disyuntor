93176108da9a6c5ed37550a56ae22810a8f34c3e
Leandro López (inkel) <inkel.ar@gmail.com>
2015-09-12 10:22:14 -0300

    Test circuit states triggering
    Yes, this somehow tests that MicroMachine is working properly. Who
    cares? :)

--- spec/disyuntor_spec.rb
+++ spec/disyuntor_spec.rb
@@ -5,6 +5,10 @@ describe Disyuntor do
   let(:circuit) { Disyuntor.new }
 
   describe "circuit closed" do
+    before do
+      circuit.close!
+    end
+
     describe "success" do
       it "should call reset"
       it "should not increment failures counter"
@@ -26,22 +30,78 @@ describe Disyuntor do
       end
     end
 
-    it "can trigger an open state"
-    it "cannot trigger a half-open state"
-    it "can trigger a close state"
+    it "can trigger an open state" do
+      circuit.open!
+
+      assert circuit.open?
+      refute circuit.closed?
+      refute circuit.half_open?
+    end
+
+    it "cannot trigger a half-open state" do
+      assert_raises(MicroMachine::InvalidState) do
+        circuit.half_open!
+      end
+
+      assert circuit.closed?
+      refute circuit.half_open?
+      refute circuit.open?
+    end
+
+    it "can trigger a close state" do
+      circuit.close!
+
+      assert circuit.closed?
+      refute circuit.half_open?
+      refute circuit.open?
+    end
   end
 
   describe "circuit open" do
+    before do
+      circuit.open!
+    end
+
     it "should not increment failures counter"
     it "should call #on_circuit_open"
     it "should not call #trip block"
     it "should not change #opened_at"
-    it "can trigger a half-open state"
-    it "cannot trigger a close state"
-    it "cannot trigger an open state"
+
+    it "can trigger a half-open state" do
+      circuit.half_open!
+
+      assert circuit.half_open?
+      refute circuit.open?
+      refute circuit.closed?
+    end
+
+    it "cannot trigger a close state" do
+      assert_raises(MicroMachine::InvalidState) do
+        circuit.close!
+      end
+
+      assert circuit.open?
+      refute circuit.closed?
+      refute circuit.half_open?
+    end
+
+    it "cannot trigger an open state" do
+      assert_raises(MicroMachine::InvalidState) do
+        circuit.open!
+      end
+
+      assert circuit.open?
+      refute circuit.closed?
+      refute circuit.half_open?
+    end
   end
 
   describe "circuit half-open" do
+    before do
+      circuit.open!
+      circuit.half_open!
+    end
+
     it "should call #try block"
 
     describe "on failure" do
@@ -55,8 +115,30 @@ describe Disyuntor do
       it "should cleanup #failures"
     end
 
-    it "can trigger open state"
-    it "can trigger close state"
-    it "cannot trigger half-open state"
+    it "can trigger open state" do
+      circuit.open!
+
+      assert circuit.open?
+      refute circuit.half_open?
+      refute circuit.closed?
+    end
+
+    it "can trigger close state" do
+      circuit.close!
+
+      assert circuit.closed?
+      refute circuit.half_open?
+      refute circuit.open?
+    end
+
+    it "cannot trigger half-open state" do
+      assert_raises(MicroMachine::InvalidState) do
+        circuit.half_open!
+      end
+
+      assert circuit.half_open?
+      refute circuit.closed?
+      refute circuit.open?
+    end
   end
 end
