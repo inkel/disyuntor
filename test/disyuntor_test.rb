a6582261f134cbfcbf6bd969f506726182dfa41b
Leandro López <inkel.ar@gmail.com>
2016-04-27 16:00:58 -0300

    Rename @cb variable to @disyuntor in tests

--- test/disyuntor_test.rb
+++ test/disyuntor_test.rb
@@ -32,174 +32,174 @@ class DisyuntorTest < Minitest::Test
   def setup
     @threshold = 3
     @timeout   = 5
-    @cb = Disyuntor.new(threshold: @threshold, timeout: @timeout)
+    @disyuntor = Disyuntor.new(threshold: @threshold, timeout: @timeout)
   end
 
   def test_initialize_closed
-    assert @cb.closed?
+    assert @disyuntor.closed?
   end
 
   def test_initialize_without_failures
-    assert_equal 0, @cb.failures
+    assert_equal 0, @disyuntor.failures
   end
 
   def test_closed_circuit_returns_block_value
-    assert_equal 42, @cb.try { 42 }
+    assert_equal 42, @disyuntor.try { 42 }
   end
 
   def test_closed_circuit_do_not_count_failures_on_success
-    @cb.try { true }
-    assert_equal 0, @cb.failures
+    @disyuntor.try { true }
+    assert_equal 0, @disyuntor.failures
   end
 
   def test_reset_failures_counter_on_closed_circuit_success
     begin
-      @cb.try { fail CustomRuntimeError }
+      @disyuntor.try { fail CustomRuntimeError }
     rescue CustomRuntimeError
     end
 
-    assert_equal 1, @cb.failures
+    assert_equal 1, @disyuntor.failures
 
-    @cb.try { true }
+    @disyuntor.try { true }
 
-    assert_equal 0, @cb.failures
+    assert_equal 0, @disyuntor.failures
   end
 
   def test_open_circuit_after_threshold_failures
-    @cb.threshold.times do
+    @disyuntor.threshold.times do
       begin
-        @cb.try { fail CustomRuntimeError }
+        @disyuntor.try { fail CustomRuntimeError }
       rescue CustomRuntimeError
       end
     end
 
-    refute @cb.closed?
+    refute @disyuntor.closed?
   end
 
   def test_open_circuit_raises_default_error
-    @cb.threshold.times do
+    @disyuntor.threshold.times do
       begin
-        @cb.try { fail CustomRuntimeError }
+        @disyuntor.try { fail CustomRuntimeError }
       rescue CustomRuntimeError
       end
     end
 
     assert_raises(Disyuntor::CircuitOpenError) do
-      @cb.try { fail CustomRuntimeError }
+      @disyuntor.try { fail CustomRuntimeError }
     end
   end
 
   def test_override_on_circuit_open
-    @cb.on_circuit_open { 42 }
+    @disyuntor.on_circuit_open { 42 }
 
-    @cb.threshold.times do
+    @disyuntor.threshold.times do
       begin
-        @cb.try { fail CustomRuntimeError }
+        @disyuntor.try { fail CustomRuntimeError }
       rescue CustomRuntimeError
       end
     end
 
-    assert_equal 42, @cb.try { fail CustomRuntimeError }
+    assert_equal 42, @disyuntor.try { fail CustomRuntimeError }
   end
 
   def test_count_failures
-    assert_equal 0, @cb.failures
+    assert_equal 0, @disyuntor.failures
 
-    @cb.threshold.times do
+    @disyuntor.threshold.times do
       begin
-        @cb.try { fail CustomRuntimeError }
+        @disyuntor.try { fail CustomRuntimeError }
       rescue CustomRuntimeError
       end
     end
 
-    assert_equal @threshold, @cb.failures
+    assert_equal @threshold, @disyuntor.failures
   end
 
   def test_do_not_count_failures_when_open
-    @cb.threshold.times do
+    @disyuntor.threshold.times do
       begin
-        @cb.try { fail CustomRuntimeError }
+        @disyuntor.try { fail CustomRuntimeError }
       rescue CustomRuntimeError
       end
     end
 
     begin
-      @cb.try { fail CustomRuntimeError }
+      @disyuntor.try { fail CustomRuntimeError }
     rescue Disyuntor::CircuitOpenError
     end
 
-    assert_equal @threshold, @cb.failures
+    assert_equal @threshold, @disyuntor.failures
   end
 
   def test_close_after_timeout_if_success
-    @cb.threshold.times do
+    @disyuntor.threshold.times do
       begin
-        @cb.try { fail CustomRuntimeError }
+        @disyuntor.try { fail CustomRuntimeError }
       rescue CustomRuntimeError
       end
     end
 
-    refute @cb.closed?
+    refute @disyuntor.closed?
 
     Time.stub(:now, Time.at(Time.now.to_i + @timeout + 1)) do
-      assert_equal 42, @cb.try { 42 }
+      assert_equal 42, @disyuntor.try { 42 }
     end
 
-    assert @cb.closed?
+    assert @disyuntor.closed?
   end
 
   def test_reopen_after_timeout_if_fails
-    @cb.threshold.times do
+    @disyuntor.threshold.times do
       begin
-        @cb.try { fail CustomRuntimeError }
+        @disyuntor.try { fail CustomRuntimeError }
       rescue CustomRuntimeError
       end
     end
 
-    refute @cb.closed?
+    refute @disyuntor.closed?
 
     Time.stub(:now, Time.at(Time.now.to_i + @timeout + 1)) do
       assert_raises(CustomRuntimeError) do
-        @cb.try { fail CustomRuntimeError }
+        @disyuntor.try { fail CustomRuntimeError }
       end
     end
 
-    refute @cb.closed?
+    refute @disyuntor.closed?
   end
 
   def test_count_failure_after_timeout_if_fails
-    @cb.threshold.times do
+    @disyuntor.threshold.times do
       begin
-        @cb.try { fail CustomRuntimeError }
+        @disyuntor.try { fail CustomRuntimeError }
       rescue CustomRuntimeError
       end
     end
 
-    refute @cb.closed?
+    refute @disyuntor.closed?
 
     Time.stub(:now, Time.at(Time.now.to_i + @timeout + 1)) do
       assert_raises(CustomRuntimeError) do
-        @cb.try { fail CustomRuntimeError }
+        @disyuntor.try { fail CustomRuntimeError }
       end
     end
 
-    assert_equal @threshold.next, @cb.failures
+    assert_equal @threshold.next, @disyuntor.failures
   end
 
   def test_close_after_timeout_if_succeeds
-    @cb.threshold.times do
+    @disyuntor.threshold.times do
       begin
-        @cb.try { fail CustomRuntimeError }
+        @disyuntor.try { fail CustomRuntimeError }
       rescue CustomRuntimeError
       end
     end
 
-    refute @cb.closed?
+    refute @disyuntor.closed?
 
     Time.stub(:now, Time.at(Time.now.to_i + @timeout + 1)) do
-      assert_equal 42, @cb.try { 42 }
+      assert_equal 42, @disyuntor.try { 42 }
     end
 
-    assert @cb.closed?
+    assert @disyuntor.closed?
   end
 end
